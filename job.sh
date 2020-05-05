616b0d655ef5441e48ca76aaa51cfdfc48b29101
Joan <joanmuntaner1999@gmail.com>
2020-05-05 17:32:52 +0200

    fix job

--- job.sh
+++ job.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 export PATH=/Soft/cuda/9.0.176/bin:$PATH
+### Directivas para el gestor de colas
+# Asegurar que el job se ejecuta en el directorio actual
+#$ -cwd
+# Asegurar que el job mantiene las variables de entorno del shell lamador
+#$ -V
+# Cambiar el nombre del job
+#$ -N SOBELEDGE
+# Cambiar el shell
+#$ -S /bin/bash
+
 nvprof --unified-memory-profiling off ./cudacode.exe
